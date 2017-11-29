eaafccdb0216ecaad45a52e6e5eec91c55661d30
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2017-11-28 17:43:23 -0700

    updates

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 console.log("Javascript is alive!");
+
+
+function replaceWord() {
+  let element = document.getElementById("greeting");
+  element.innerHTML = "Hello, World!";
+}
+replaceWord()
+
+
+
+function changeColor() {
+  let targets = document.querySelectorAll('li');
+  for (let i = 0; i < targets.length; i++) {
+    targets[i].style.backgroundColor = "yellow";
+  }
+}
+changeColor()
+
+
+let newimg = document.createElement("img");
+newimg.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+document.getElementById('greeting').appendChild(newimg);
+
+
+Element.prototype.addClassName = function (selected) {
+  if (!this.hasClassName(selected)) {
+    this.className = [this.className,]
+  }
+}
