9dc143f345c2d9790b2ad88f05d52b1da1d4686f
Jennifer Tristan <31359254+jt6161@users.noreply.github.com>
2017-11-29 18:14:08 -0700

    updates with jquery

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
-console.log("Javascript is alive!");
+// function replaceWord() {
+//   let element = document.getElementById("greeting");
+//   element.innerHTML = "Hello, World!";
+// }
+// replaceWord()
+//
+// function changeColor() {
+//   let targets = document.querySelectorAll('li');
+//   for (let i = 0; i < targets.length; i++) {
+//     targets[i].style.backgroundColor = "yellow";
+//   }
+// }
+// changeColor()
+//
+// let newimg = document.createElement("img");
+// newimg.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
+// document.getElementById('greeting').appendChild(newimg);
+//
+// function addClass ()
+// let list = document.querySelectorAll(li)
+// for (let i = 0; i < list.length; i++) {
+//   list.addEventListener("click", function() {
+//       if (list[i].className !== "selected") {
+//         list[i].className = "selected"
+//       }
+//     }
+//   }
+//   addClass()
+
+$(document).ready(function() {
+
+$('#greeting').text("HELLO WORLD!");
+
+$("li").css('background-color', "yellow");
+
+let newimg = $('<img>');
+newimg.attr({'src': 'http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif'})
+.appendTo("#greeting");
 
 
-function replaceWord() {
-  let element = document.getElementById("greeting");
-  element.innerHTML = "Hello, World!";
-}
-replaceWord()
-
-
-
-function changeColor() {
-  let targets = document.querySelectorAll('li');
-  for (let i = 0; i < targets.length; i++) {
-    targets[i].style.backgroundColor = "yellow";
-  }
-}
-changeColor()
-
-
-let newimg = document.createElement("img");
-newimg.src = "http://49.media.tumblr.com/tumblr_m6qt1rjPSz1rxjzkho1_500.gif";
-document.getElementById('greeting').appendChild(newimg);
-
-
-document.querySelector(li).classList.add(selected);
+})
